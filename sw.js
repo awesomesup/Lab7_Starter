f7a1e4bb6abf49fc354254101f8e8926362c4a08
awesomesup <77423340+awesomesup@users.noreply.github.com>
2022-11-14 17:46:16 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -37,4 +37,4 @@ self.addEventListener('fetch', function (event) {
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
-});
+}); 
